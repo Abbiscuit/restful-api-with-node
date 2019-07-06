dd9e69c61ab8c016c7f982c60f014b8e44bf2bb7
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-07-07 00:19:49 +0900

    Add post function with async/await

--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,28 @@ router.get('/', (req, res) => {
   res.send('We are on POST!');
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body.title);
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  // post
+  //   .save()
+  //   .then(data => {
+  //     res.json(data);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.json({ message: err });
+  //   });
+
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
